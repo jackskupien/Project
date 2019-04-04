a1e31750ccef7f6fb729c6eeed52e4ce2c2966a0
Jack Skupien <49174783+jackskupien@users.noreply.github.com>
2019-04-04 10:38:24 -0500

    Add files via upload

---
+++ GinRummy/GinRummy/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Creators: Jack Skupien & Reid Leidheiser
+ * Date: 04/04/2019
+ * Time: 10:25 AM
+ * Purpose: To simulate a two-player game of Gin Rummy
+ * Contributors: None
+ */
+using System;
+namespace GinRummy{
+	class Program{
+		public static void Main(string[] args){
+			Console.ReadKey(true);
+		}//End Main
+	}//End Class
+}//End Namespace
